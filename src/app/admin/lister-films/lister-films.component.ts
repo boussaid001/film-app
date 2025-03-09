e8c977a89289e5a73f1f0ab9cdbfa36895e7257e
boussaid001 <mohamedamine.boussaid@polytechnicien.tn>
2025-03-09 01:55:26 +0100

    Pushing V1.0

--- src/app/admin/lister-films/lister-films.component.ts
+++ src/app/admin/lister-films/lister-films.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {Film} from "../../shared/models/film";
+import {FilmsService} from "../../services/films.service";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-lister-films',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './lister-films.component.html',
   styleUrl: './lister-films.component.css'
 })
 export class ListerFilmsComponent {
 
+  tabFilms :Film[] = [];
+  constructor( private filmsService: FilmsService ) {
+  }
+  ngOnInit(): void {
+    this.tabFilms = this.filmsService.getFilms();
+
+  }
+  supprimer(id : number){
+    this.filmsService.deleteFilm(id)
+  }
+
+  confirmAndDelete(id: number){
+    if (confirm("Are you sure you want to delete this film?")){
+      this.filmsService.deleteFilm(id)
+    }
+  }
 }
